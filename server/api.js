d9e25595e527ff5334345a571ca1030381e12b23
Anandu YD <ananduyd.uk@gmail.com>
2019-05-09 17:43:10 +0100

    test-run

--- server/api.js
+++ server/api.js
@@ -7,9 +7,51 @@ import * as db from "./data/models";
 
 //routes
 routerApi.get("/deals", async (req, res) => {
-    db.dealsCollection.find().exec(function(err, result) {
-        res.send(result);
-    });
+
+    const {title} = req.query;
+    if (title) {
+        db.dealsCollection.aggregate([{$match: {title}}]).exec((err, result) => {
+            if (err)
+                console.log("error", err);
+            res.status(500);
+            res.status(200).send(result);
+        })
+    } else
+        db.dealsCollection.find().exec(function (err, result) {
+            res.send(result);
+        });
 });
+routerApi.get("/deal/:id", async (req, res) => {
+    const {id} = req.params;
+    if (id)
+        db.dealsCollection.find({_id: id}).exec((err, result) => {
+            if (err)
+                console.log("error", err);
+            res.status(500);
+            res.status(200).send(result);
+        })
+    else
+        res.status(200).send({message: "ID missing"});
+
+})
+routerApi.get("/deals/stats", async (req, res) => {
+
+    db.dealsCollection.aggregate([
+        {$count: "deals_count"},
+        // {
+        //     $group:
+        //         {
+        //             totalAmount: {$sum: ["amount",]},
+        //             count: { $sum: ["amount"]}
+        //         }
+        // }
+    ]).exec((err, result) => {
+        if (err)
+            console.log("error", err);
+        res.status(200).send(result);
+    });
+
+})
+
 
 export default routerApi;
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import fetch from "isomorphic-fetch";
 import logo from "./logo.svg";
 import "./App.css";
@@ -7,33 +7,69 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deals: []
+            deals: [],
+            stats: ''
         }
     }
-    
-  componentDidMount() {
-    fetch(`http://localhost:3001/api/deals`)
-        .then(response => response.json())
-        .then(response => {
-            this.setState({ deals: response });
-        });
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-        </header>
 
-        <main className="BodyContent">
-            <h1>Deals</h1>
-            <div>
-                ...
+    async componentDidMount() {
+        await fetch(`http://localhost:3001/api/deals`)
+            .then(response => response.json())
+            .then(response => {
+                this.setState({deals: response});
+            });
+        await fetch(`http://localhost:3001/api/deals/stats`)
+            .then(response => response.json())
+            .then(response => {
+                this.setState({stats: response[0]});
+            });
+    }
+
+    onClickSearch() {
+        const {search} = this.state;
+        if (search !== '') {
+            fetch(`http://localhost:3001/api/deals?title=${search}`)
+                .then(response => response.json())
+                .then(response => {
+                    this.setState({deals: response});
+                });
+        }
+    }
+
+    render() {
+        const {deals, stats} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+                <h5>Total Deals: {stats !== '' ? stats.deals_count : 0}</h5>
+
+                <main className="BodyContent">
+                    <h1>Deals</h1>
+                    <div>
+                        <input placeholder="Search title here" onChange={(e) => {
+                            this.setState({search: e.target.value})
+                        }}/>
+                        <button onClick={() => this.onClickSearch()}>Search</button>
+                    </div>
+                    <div>
+                        <ul>
+                            {
+                                deals.length > 0 ?
+                                    deals.map((deal, index) => (
+                                        <li key={index}>
+                                            <h6>{deal.title}</h6>
+                                        </li>
+                                    )) :
+                                    <li>Fetching...</li>
+                            }
+                        </ul>
+                    </div>
+                </main>
             </div>
-        </main>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
