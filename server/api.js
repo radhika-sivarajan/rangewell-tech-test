900df9ca4761c4fb4c2c1ff61330c93895db175a
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-05-20 00:14:15 +0100

    New api endpoints for deals and stat

--- server/api.js
+++ server/api.js
@@ -7,7 +7,32 @@ import * as db from "./data/models";
 
 //routes
 routerApi.get("/deals", async (req, res) => {
-    db.dealsCollection.find().exec(function(err, result) {
+    let title = req.query.title; 
+    if( title == undefined){
+        db.dealsCollection.find().exec(function(err, result) {
+            res.send(result);
+        });
+    }else{
+        db.dealsCollection.find({ 'title' : title }).exec(function(err, result) {
+            res.send(result);
+        });
+    }
+});
+
+// Return a specific stat data
+routerApi.get("/deals/stats", async (req, res) => {
+    let statistics = {
+        deals_count: 200,       // number of deals
+        total_amounts: 200000,  // total amountRequired of deals
+        avg_amount: 3000        // average of deals  amountRequired
+    };
+    res.send(statistics);
+});
+
+// Get deal by it's id
+routerApi.get("/deals/:id", async (req, res) => {
+    let id = req.params.id; 
+    db.dealsCollection.findById(id).exec(function(err, result) {
         res.send(result);
     });
 });
