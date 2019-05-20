e468d85f5a3a3c484090844b726cf1456dc8e5da
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-05-20 02:05:32 +0100

    Show stats of deals on top

--- src/App.js
+++ src/App.js
@@ -4,43 +4,63 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const API = 'http://localhost:3001/api/';
-const ALL_DEAL_QUERY = 'deals';
+const DEALS_QUERY = 'deals/';
+const STATS_QUERY = 'stats/';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            deals: []
-        }
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+          deals: [],
+          stats: []
+      }
+  }
     
   componentDidMount() {
-    fetch(API + ALL_DEAL_QUERY)
-        .then(response => response.json())
-        .then(response => {
-            this.setState({ deals: response });
-        });
+    fetch(API + DEALS_QUERY)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ deals: response });
+      });
+
+    fetch(API + DEALS_QUERY + STATS_QUERY)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({ stats: response });
+    });
   }
 
+  // Method to render deal component
   renderDeals = () => {
-    console.log(this.state.deals)
-    const {deals} = this.state;
     let dealRows;
-    if (deals.length !== 0) {
-      let sortedByDateDeal = deals.sort(function(a,b){ 
+    if (this.state.deals.length !== 0) {
+      let sortedByDateDeal = this.state.deals.sort(function(a,b){ 
         return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime() 
       });
       dealRows = sortedByDateDeal.map((item, index) => {
         return (
-            <tr key={index}>
-                <td>{item.title}</td>
-                <td>{item.amountRequired}</td>
-                <td>{item.createdAt}</td>
-            </tr>
+          <tr key={index}>
+              <td>{item.title}</td>
+              <td>{item.amountRequired}</td>
+              <td>{item.createdAt}</td>
+          </tr>
         );
       });
     }
     return dealRows;
+  }
+
+  // Method to render stat component
+  renderStats = () => {
+    console.log(this.state.stats)
+    let statData;
+    if (this.state.stats.length !== 0) {
+      return(
+        <div>
+          <p><b>Deals count : </b>{this.state.stats.deals_count},<b> Total amounts : </b>{this.state.stats.total_amounts},<b> Avg amount : </b>{this.state.stats.avg_amount}</p>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -50,10 +70,16 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
         </header>
         <p></p>
+        <div className="row text-center stat-info">
+          <div className="col-12">
+          <h5><p className="text-danger">Stats</p></h5>
+          {this.renderStats()}
+          </div>
+        </div>
         <div className="row text-left">
-          <div className="col-8">
+          <div className="col-7">
             <main>
-              <h3><p>Deals</p></h3>
+              <h3 className="text-info"><p>Deals</p></h3>
               <table className="table table-hover">
                   <thead className="table-info">
                     <tr>
